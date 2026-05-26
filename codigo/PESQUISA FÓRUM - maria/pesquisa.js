fe0ae52da6fed956684cec26ecfd6062cca4ea96
Maria Luiza Queiroz <malumartins090606@gmail.com>
2026-05-26 20:01:20 -0300

    adicionando o json

---
+++ codigo/PESQUISA FÓRUM - maria/pesquisa.js
@@ -0,0 +1,27 @@
+let posts = JSON.parse(localStorage.getItem("posts")) || [
+
+  {
+    id: 1,
+    titulo: "Tenho medo de denunciar meu parceiro",
+    descricao: "Ele ameaça pegar meu celular e diz que ninguém vai acreditar em mim.",
+    categoria: "Relacionamento abusivo",
+    respostas: 12
+  },
+
+  {
+    id: 2,
+    titulo: "Estou recebendo ameaças",
+    descricao: "Meu ex está me perseguindo pelas redes sociais e mandando mensagens.",
+    categoria: "Ameaça",
+    respostas: 8
+  },
+
+  {
+    id: 3,
+    titulo: "Preciso de ajuda psicológica",
+    descricao: "Estou me sentindo muito pressionada emocionalmente e não sei o que fazer.",
+    categoria: "Ajuda psicológica",
+    respostas: 5
+  }
+
+];
