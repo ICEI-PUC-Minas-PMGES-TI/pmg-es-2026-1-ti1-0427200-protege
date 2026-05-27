95e32c75f94a6ebce08cbb1e7c3b881267ddd228
giovanafariaa <giovanafmartins024@gmail.com>
2026-05-27 19:59:00 -0300

    Atualização do js, parte das provas

--- codigo/page-detalhes-provas/script.js
+++ codigo/page-detalhes-provas/script.js
@@ -7,6 +7,69 @@ async function carregarProvas(){
     await fetch(
         "http://localhost:3000/provas"
     );
+const provas =
+    await resposta.json();
 
+    lista.innerHTML = "";
+
+    provas.forEach((prova) => {
+
+        let preview = "";
+
+        if(
+            prova.arquivoTipo === "imagem"
+        ){
+
+            preview = `
+                <img
+                    src="${prova.arquivo}"
+                    class="preview-img"
+                >
+            `;
+        }
+
+        else if(
+            prova.arquivoTipo === "video"
+        ){
+
+            preview = `
+                <video
+                    controls
+                    class="preview-video"
+                >
+                    <source
+                        src="${prova.arquivo}"
+                    >
+                </video>
+            `;
+        }
+
+        else if(
+            prova.arquivoTipo === "audio"
+        ){
+
+            preview = `
+                <audio
+                    controls
+                    class="audio-player"
+                >
+                    <source
+                        src="${prova.arquivo}"
+                    >
+                </audio>
+            `;
+        }
+
+        else{
+
+            preview = `
+                <iframe
+                    src="${prova.arquivo}"
+                    class="preview-pdf"
+                >
+                </iframe>
+            `;
+        }
     
     }
+}
