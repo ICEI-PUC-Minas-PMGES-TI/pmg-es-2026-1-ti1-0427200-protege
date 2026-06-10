1dd0018a9a975cfad1515984dba2dffb987457b2
Maria Luiza Queiroz <malumartins090606@gmail.com>
2026-06-10 19:07:02 -0300

    finalizando a parte dos post de respostas

--- codigo/PESQUISA FÓRUM - maria/post.js
+++ codigo/PESQUISA FÓRUM - maria/post.js
@@ -11,7 +11,8 @@ const posts = [
         categoria: "Denúncia",
         titulo: "Tenho medo de denunciar meu parceiro",
         descricao:
-            "Ele me ameaça constantemente e tenho medo das consequências caso eu denuncie."
+            "Ele me ameaça constantemente e tenho medo das consequências caso eu denuncie.",
+        respostas: 12
     },
 
     {
@@ -19,7 +20,8 @@ const posts = [
         categoria: "Ameaça",
         titulo: "Estou recebendo ameaças",
         descricao:
-            "Meu ex está me perseguindo pelas redes sociais e enviando mensagens agressivas."
+            "Meu ex está me perseguindo pelas redes sociais e enviando mensagens agressivas.",
+        respostas: 8
     },
 
     {
@@ -27,10 +29,75 @@ const posts = [
         categoria: "Ajuda psicológica",
         titulo: "Preciso de ajuda psicológica",
         descricao:
-            "Estou emocionalmente esgotada e preciso de orientação."
+            "Estou emocionalmente esgotada e preciso de orientação.",
+        respostas: 5
+    },
+
+    {
+        id: "4",
+        categoria: "Denúncia",
+        titulo: "Quero denunciar mas tenho medo",
+        descricao:
+        "Tenho provas contra meu companheiro, mas estou com medo das consequências da denúncia.",
+        respostas: 14
+    },
+
+    {
+        id: "5",
+        categoria: "Denúncia",
+        titulo: "Minha família não acredita em mim",
+        descricao:
+        "Contei sobre as agressões que sofro, mas minha família acha que estou exagerando.",
+        respostas: 9
+    },
+
+    {
+        id: "6",
+        categoria: "Ameaça",
+        titulo: "Estou sendo ameaçada pelo meu ex",
+        descricao:
+        "Depois do término, meu ex começou a mandar mensagens agressivas e aparecer perto da minha casa.",
+        respostas: 11
+    },
+
+    {
+        id: "7",
+        categoria: "Relacionamento abusivo",
+        titulo: "Não consigo sair desse relacionamento",
+        descricao:
+        "Mesmo sabendo que estou em um relacionamento abusivo, tenho medo de terminar e ficar sozinha.",
+        respostas: 16
+    },
+
+    {
+        id: "8",
+        categoria: "Ajuda psicológica",
+        titulo: "Estou emocionalmente esgotada",
+        descricao:
+        "As situações que venho passando estão afetando minha saúde mental e preciso de apoio psicológico.",
+        respostas: 7
     }
 
 ];
+
+const comentarios = {
+
+    1: [
+        "Você não está sozinha. Procure ajuda especializada.",
+        "Tente reunir provas e buscar apoio de pessoas de confiança."
+    ],
+
+    2: [
+        "Guarde prints das mensagens.",
+        "Considere registrar um boletim de ocorrência."
+    ],
+
+    3: [
+        "Procure atendimento psicológico gratuito na sua região.",
+        "Conversar com alguém de confiança pode ajudar."
+    ]
+
+};
 
 const post = posts.find(
     p => p.id === id
@@ -48,6 +115,11 @@ if(post){
                 ${post.categoria}
             </span>
 
+            <div class="info-post">
+                <span> Anônimo</span>
+                <span> 10/06/2026</span>
+            </div>
+
             <h1>
                 ${post.titulo}
             </h1>
@@ -56,7 +128,42 @@ if(post){
                 ${post.descricao}
             </p>
 
+            <div class="respostas-info">
+                 ${post.respostas} respostas
+            </div>
+
         </div>
 
     `;
+
+    carregarComentarios();
+}
+
+function carregarComentarios(){
+
+    const lista =
+    document.getElementById(
+        "lista-comentarios"
+    );
+
+    lista.innerHTML = "";
+
+    if(comentarios[id]){
+
+        comentarios[id].forEach(
+            comentario => {
+
+                lista.innerHTML += `
+
+                    <div class="comentario">
+                        ${comentario}
+                    </div>
+
+                `;
+
+            }
+        );
+
+    }
+
 }
