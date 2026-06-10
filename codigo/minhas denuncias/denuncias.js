c8d6adc0d682818a4b6ee2813b3a7d435fc46f07
Anna Luiza Pereirs Silva <annalumodascolonial@gmail.com>
2026-06-10 14:25:29 -0300

    consertando lista js

--- codigo/minhas denuncias/denuncias.js
+++ codigo/minhas denuncias/denuncias.js
@@ -28,3 +28,66 @@ const denuncias = [
     protocolo: "DEN-2026-004"
   }
 ];
+const lista = document.getElementById("listaDenuncias");
+
+denuncias.forEach((denuncia) => {
+
+  let classeStatus = "";
+  let classeIcone = "";
+  let icone = "";
+
+  switch (denuncia.status) {
+    case "Em andamento":
+      classeStatus = "andamento";
+      classeIcone = "vermelho";
+      icone = "!";
+      break;
+
+    case "Em avaliação":
+      classeStatus = "avaliacao";
+      classeIcone = "amarelo";
+      icone = "?";
+      break;
+
+    case "Concluída":
+      classeStatus = "concluida";
+      classeIcone = "verde";
+      icone = "✓";
+      break;
+
+    case "Arquivada":
+      classeStatus = "arquivada";
+      classeIcone = "roxo";
+      icone = "✕";
+      break;
+  }
+  lista.innerHTML += `
+    <div class="card">
+
+      <div class="esquerda">
+
+        <div class="icone ${classeIcone}">
+          ${icone}
+        </div>
+
+        <div class="info">
+          <h2>${denuncia.titulo}</h2>
+          <p>Registrada em ${denuncia.data}</p>
+          <span class="tipo">${denuncia.tipo}</span>
+        </div>
+
+      </div>
+
+      <div class="direita">
+        <span class="status ${classeStatus}">
+          ${denuncia.status}
+        </span>
+
+        <p class="protocolo">
+          Protocolo: ${denuncia.protocolo}
+        </p>
+      </div>
+
+    </div>
+  `;
+});
